bd11fc9ab7c635a36c630a0544aa56de6b43ddb8
ildac <marco@dacol.me>
2014-11-12 17:57:28 +0100

    test case for user subscriptino written

--- routes/edition.js
+++
@@ -1,9 +0,0 @@
-
-var router = require('express').Router();
-var editionController = require('../controllers/edition');
-
-router.route('/editions/:editionId')
-    .put(editionController.putEdition);
-
-
-module.exports = router;
--- test/registrant.js
+++ test/registrant.js
@@ -16,43 +16,162 @@ var Registrant = require('../models/registrant');
 
 describe('Routing for course edition registration', function () {
     var url = 'http://localhost:8000/api/v1';
+
     var userId;
     var courseId;
     var editionId;
+
+    var basePath;
 
     // Setup the scenario, create a user a course and a course edition
     before(function (done) {
         mongoose.connect(config.db.mongodb);
 
-        // TODO setup!
+        // Setup:
+        //  - create the user and insert it into the DB
+        //  - create the course and the edition and insert them into the DB
+        //  - get all the IDs
+
+        var userVal = {
+            cardNumber: '00000001',
+            emails: {
+                email: 'anne@bob.fake'
+            },
+            password: 'test',
+            personalData: {
+                name: 'Anne',
+                surname: 'Bob',
+                dateOfBirth: new Date('1980', '2', '20'),
+                cityOfBirth: 'Paris',
+                provinceOfBirth: 'Paris',
+                address: {
+                    street: 'Tour Eiffel',
+                    number: '10',
+                    city: 'Paris',
+                    province: 'Paris',
+                    CAP: '75001'
+                },
+                baptizedInParish: 'Notre Dam'
+            },
+            phones: [{
+                number: '045871928392',
+                name: 'Reception'
+            }],
+            scuola: {
+                year: '1990',
+                name: 'One School oround there',
+                class: 'First one',
+                section: 'C'
+            },
+            note: {
+                text: 'I\'m Red',
+                date: new Date('2014', '5', '20')
+            }
+        };
+        var courseVal = {
+            name: 'MochaTest',
+            description: 'mochaTest Course!'
+        };
+        var editionVal = {
+            year: "2000/12",
+            startingDate: "2014-09-01T00:00:00.000Z",
+            endingDate: "2015-06-30T00:00:00.000Z"
+        };
+
+        var user = new User(userVal);
+        var course = new Course(courseVal);
+        var edition = new Edition(editionVal);
+
+        user.save(function (err, savedUser) {
+            userId = savedUser._id;
+        });
+
+        course.editions.push(edition);
+        course.save(function (err, savedCourse) {
+            courseId = savedCourse._id;
+            editionId = savedCourse.editions._id;
+        });
+
+        basePath = '/courses/' + courseId + '/editions/' + editionId + '/registrants/';
 
         done();
     });
 
     it('should register the user', function (done) {
+        request(url)
+            .post('/courses/' + courseId + '/editions/' + editionId + '/registrants/')
+            .send({userId: userId})
+            .end(function (err, res) {
+                if (err) {
+                    throw err;
+                }
 
+                res.should.have.status(200);
+                done();
+            })
     });
 
     it('should return "true" if the user is registered "false" otherwise', function (done) {
+        request(url)
+            .get(basePath + userId)
+            .end(function (err, res) {
+                if (err) {
+                    throw err;
+                }
 
+                res.body.message.should.equal('true');
+
+                done();
+            });
     });
 
     it('should return the list of all the user registered', function (done) {
+        request(url)
+            .get(basePath)
+            .end(function (err, res) {
+                if(err) {
+                    throw err;
+                }
 
+                res.body.data.registrants[0].userId.should.equal(userId.toString());
+                res.body.data.number.should.equal('1');
+                done()
+            });
     });
 
     it('should change the status of the registration to confirmed', function (done) {
+        request(url)
+            .put(basePath + userId)
+            .end(function (err, res) {
+                if(err) {
+                    throw err;
+                }
 
+                res.body.data.registrant.confirmed.should.equal(true);
+
+                done();
+            });
     });
 
     it('should delete the user registration', function (done) {
+        request(url)
+            .delete(basePath + userId)
+            .end(function(err, res) {
+                if (err) {
+                    throw err;
+                }
 
+                res.should.have.status(200);
+                done();
+            })
     });
 
     // Scenario cleanup, delete edition, course and user. Disconnect from DB
     after(function (done) {
 
-        //TODO cleanup!
+        // Delete from the db the fake inserted entity
+        Course.findByIdAndRemove(courseId);
+        User.findByIdAndRemove(userId);
 
         mongoose.disconnect();
         done();
